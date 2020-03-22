dcfb639324b1c2516836616589f8ac2b5dc0694f
Sam <michalconstantine@gmail.com>
2020-03-22 01:26:23 +0100

    Initial upload

---
+++ src/main/java/cz/hydracore/electrovanilla/ElectroVanilla.java
@@ -0,0 +1,16 @@
+package cz.hydracore.electrovanilla;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ElectroVanilla extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
